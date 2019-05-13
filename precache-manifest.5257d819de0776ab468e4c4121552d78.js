42264d559dc0b5745b3041558d15d0402cf92434
Seipold <sullyseipold@gmail.com>
2019-05-13 19:04:30 -0400

    Updates

---
+++ precache-manifest.5257d819de0776ab468e4c4121552d78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fea98e273f88b08dd8b466ebb20dda0",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "8782e1d281be76c2105c",
+    "url": "/react-gh-pages/static/css/main.51e1e065.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/react-gh-pages/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "8782e1d281be76c2105c",
+    "url": "/react-gh-pages/static/js/main.e9f95923.chunk.js"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.cae92349879c43c936844ca6b9055730.js"
+  "/react-gh-pages/precache-manifest.5257d819de0776ab468e4c4121552d78.js"
 );
 
 self.addEventListener('message', (event) => {
